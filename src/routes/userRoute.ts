ae4d0fd5fd39cf2ab083a7b2748a0f8d26990bee
michael harush <michaelharush551@gmail.com>
2023-02-28 12:33:29 +0200

    Add swagger to userRoute

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -8,8 +8,48 @@ import user from '../controllers/user'
 import express, { NextFunction, Request, Response } from 'express'
 const router = express.Router()
 
+/**
+ * @swagger
+ * /user/{id}:
+ *   get:
+ *     summary: Get user by ID
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: User ID
+ *     responses:
+ *       '200':
+ *         description: User details
+ *       '404':
+ *         description: User not found
+ */
 router.get('/:id', user.getUserById)
 
+/**
+ * @swagger
+ * /user:
+ *   put:
+ *     summary: Update a user
+ *     tags: [User]
+ *     requestBody:
+ *       description: User object to update
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       '200':
+ *         description: User updated successfully
+ *       '400':
+ *         description: Bad request
+ *       '404':
+ *         description: User not found
+ */
 router.put('/',user.upadteUser)
 
 export = router
