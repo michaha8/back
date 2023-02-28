11d2fb9d9ac3417a2a7d0a891548d67fe6b80a41
michael harush <michaelharush551@gmail.com>
2023-02-28 12:32:10 +0200

    Add swager to index route

--- src/routes/indexRoute.ts
+++ src/routes/indexRoute.ts
@@ -8,6 +8,21 @@
 import express from 'express'
 const router = express.Router()
 
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Returns a welcome message
+ *     tags: [Index]
+ *     responses:
+ *       200:
+ *         description: A welcome message
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: string
+ *             example: Hello world!!
+ */
 router.get('/',(req, res)=>{
     res.send('Hello world!!')
 })
